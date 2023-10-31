28a98c48262b9c40f64271e9d3a2a9271d9c4dca
mey <meydetour@gmail.com>
2023-10-31 10:43:42 +0100

    y

--- signup/signup.js
+++ signup/signup.js
@@ -1,18 +1,32 @@
-
 let page1Bouton = document.querySelector('.icone-next')
-let page2= document.querySelector('.page2')
+let page2 = document.querySelector('.page2')
 let page1 = document.querySelector('.page1')
+const textArray = ['Bienvenue sur notre plateforme créative !', 'Ici, chaque utilisateur est une star de la créativité. Vous avez un compte personnel pour partager vos projets, vos idées, et interagir avec une communauté passionnée. ',
+    'Publiez vos créations, recevez des retours et améliorez-les grâce à la fonction "ReDo" qui vous permet de collaborer et de perfectionner ensemble.',
+    ' Notre site encourage le partage public, car nous croyons en l\'inspiration collective. Explorez un monde de possibilités, personnalisez votre profil et découvrez une révolution de la créativité où l\'enthousiasme est le moteur principal. Rejoignez-nous pour créer, collaborer et vous épanouir !']
 
-page1Bouton.addEventListener('click',()=>{
+
+page1Bouton.addEventListener('click', () => {
     page2.style.width = '70%'
     page2.style.transform = 'translateX(-72%)'
-    page2.style.transition= '1s'
+    page2.style.transition = '1s'
     page1.style.transform = 'translateX(-100%)'
 })
 
-const textElement = document.getElementById('text');
-const cursorElement = document.getElementById('cursor');
+let textSPace = document.getElementById('text');
+let cpt = 0
+page1Bouton.addEventListener('click', revealText)
 
-textElement.addEventListener('animationend', () => {
-    cursorElement.style.display = 'none';
-});
+
+function revealText() {
+    textArray.forEach((paragraphe) => {
+        let caracters = paragraphe.split('')
+        caracters.forEach((lettre) => {
+                textSPace.innerHTML += lettre
+        })
+
+        textSPace.innerHTML += '<br><br>'
+        cpt++
+    })
+
+}
