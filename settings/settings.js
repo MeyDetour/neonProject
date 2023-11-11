687ec645a18220e59aa0fe70dd65a1355cba80d2
mey <meydetour@gmail.com>
2023-11-11 17:27:13 +0100

    zz

--- settings/settings.js
+++ settings/settings.js
@@ -1,3 +1,9 @@
 
 
 document.querySelectorAll('.settings-option').forEach((option)=>{ option.addEventListener('click',()=>{ document.querySelector('.setting-container-menu').style.height = document.getElementById(option.getAttribute('href').substring(1)).offsetHeight + 'px' })})
+
+fetch("https://api.github.com/users/MeyDetour#")
+.then(response => response.json())
+.then(data =>{
+
+})
